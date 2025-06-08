b7a47e673e6c493fa25a24de44afc09226ee6778
jnvc583 <jnvcotc@zohomail.cn>
2025-06-08 08:31:40 +0800

    Create charz.js

---
+++ charz.js
@@ -0,0 +1,40 @@
+      var btn= document.getElementById("btn");
+      btn.onclick=function(){
+        if(af.pn2.value == "亿口子赫" || af.pn2.value == "陈睿铭"){
+          alert("亿口子赫，亿口组的组长。男，原名陈睿铭。亿口组成员。擅长画漫画、英语（虽然他在读小学，但其英语能力可以参加高考）。");
+        } else if(af.pn2.value == "亿口叶子" || af.pn2.value == "叶城宇"){
+          alert("亿口叶子，亿口子赫的cp。男，原名叶城宇。亿口组成员。现在不在我们身边，在广州上学。喜欢造孽。");
+        } else if(af.pn2.value == "亿口小锐" || af.pn2.value == "杨锐"){
+          alert("亿口小锐，亿口子赫的cp。男，原名杨锐。亿口组成员。戴眼镜👓，喜欢挠别人。");
+        } else if(af.pn2.value == "亿口星辰" || af.pn2.value == "郭梓宸"){
+          alert("亿口星辰，亿口子赫的cp。男，原名郭梓宸。亿口组成员。");
+        } else if(af.pn2.value == "亿口面具"){
+          alert("亿口面具，亿口子赫的cp。男。爱好杀人。亿口组成员。戴上面具很恐怖，摘下来就不恐怖了。");
+        } else if(af.pn2.value == "亿口幽蓝" || af.pn2.value == "宁政源"){
+          alert("亿口幽蓝。男，原名宁政源。亿口组成员。爱好搞科技。");
+        } else if(af.pn2.value == "亿口尘埃"){
+          alert("亿口尘埃。男。性格好胜。亿口组成员。");
+        } else if(af.pn2.value == "亿口老六" || af.pn2.value == "熊佳煜"){
+          alert("亿口老六，大家共同的敌人。男，原名熊佳煜。典型的反派。");
+        } else if(af.pn2.value == "卷毛"){
+          alert("卷毛，亿口老六的科研人员。男。");
+        } else if(af.pn2.value == "黑涩会缺缺缺"){
+          alert("黑涩会缺缺缺，大家共同的老师。男。战力天花板。");
+        } else if(af.pn2.value == "玖悦314" || af.pn2.value == "叶雨欣"){
+          alert("玖悦314，亿口子赫的girlfriend。女，原名叶雨欣。亿口组成员。脾气不太好");
+        } else if(af.pn2.value == "黑客老八" || af.pn2.value == "陈韵泽"){
+          alert("黑客老八，黑客组的组长。男，原名陈韵泽。黑客组成员。擅长信息技术和数学，这个网页就是他写的。");
+        } else if(af.pn2.value == "黑客图形君"){
+          alert("黑客图形君（10人）。全是男的。黑客组成员。");
+        } else if(af.pn2.value == "亿口组"){
+          alert("亿口组，战斗系列。成员有亿口子赫、亿口叶子、亿口小锐、亿口星辰、亿口面具、亿口幽蓝、亿口尘埃");
+        } else if(af.pn2.value == "黑客组"){
+          alert("黑客组，科技系列。成员有黑客老八、黑客图形君");
+        } else if(af.pn2.value == "jnvc583.github.io"){
+          alert("请浏览");
+        } else if(af.pn2.value == ""){
+          alert("我们不是无名氏");
+        } else {
+          alert("没有该组/人物");
+        }
+      }
