bc5facaf2ddc23e49b12102e721e08daa07091d2
Yunze Chen <jnvcotc@zohomail.cn>
2025-08-30 15:09:57 +0800

    Delete gem.js

--- gem.js
+++
@@ -1,9 +0,0 @@
-var etn= document.getElementById("etn");
-etn.onclick=function(){
-  Properties props=new Properties();
-  props.put("mail.smtp.host", "stmp.zohp.com");
-  props.put("mail.smtp.port", "465");
-  props.put("mail.smtp.auth", "true");
-  public final class JavaMailUtils {
-  }
-}
